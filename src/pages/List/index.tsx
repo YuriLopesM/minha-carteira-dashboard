258b13365f587432786573ea881d5f30e8551393
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2021-02-02 16:40:25 -0300

    Update README

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,20 +4,43 @@ import ContentHeader from '../../components/ContentHeader'
 import SelectInput from '../../components/SelectInput'
 import HistoryFinanceCard from '../../components/HistoryFinanceCard'
 
-import { Container, Content } from './styles'
+import { Container, Content, Filters } from './styles'
 
 const List: React.FC = () => {
-    const options = [
-        { value: 'Rodrigo', label: 'Rodrigo' },
-        { value: 'Rodrigo', label: 'Rodrigo' },
-        { value: 'Rodrigo', label: 'Rodrigo' },
+    const months = [
+        { value: 7, label: 'Julho' },
+        { value: 8, label: 'Agosto' },
+        { value: 9, label: 'Setembro' },
+    ]
+
+    const years = [
+        { value: 2021, label: 2021 },
+        { value: 2020, label: 2020 },
+        { value: 2019, label: 2019 },
+        { value: 2018, label: 2018 },
     ]
 
     return (
         <Container>
             <ContentHeader title="Saídas" lineColor="#E44C4E ">
-                <SelectInput options={options} />
+                <SelectInput options={months} />
+                <SelectInput options={years} />
             </ContentHeader>
+
+            <Filters>
+                <button 
+                type="button"
+                className="tag-filter tag-filter-recurrent"
+                >
+                    Recorrentes
+                </button>
+                <button 
+                type="button"
+                className="tag-filter tag-filter-eventual"
+                >
+                    Eventuais
+                </button>
+            </Filters>
 
             <Content>
                 <HistoryFinanceCard
@@ -26,79 +49,6 @@ const List: React.FC = () => {
                 subtitle="12/12/2012"
                 amount="R$ 130,00"
                 />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                <HistoryFinanceCard
-                tagColor = '#E44C4E'
-                title="Conta de Luz"
-                subtitle="12/12/2012"
-                amount="R$ 130,00"
-                />
-                
                 
             </Content>
         </Container>
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -7,3 +7,38 @@ export const Container = styled.div`
 export const Content = styled.div`
     
 `;
+
+export const Filters = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    .tag-filter{
+        font-size: 18px;
+        font-weight: 500;
+        background: none;
+        color: ${props => props.theme.colors.white};
+        margin: 0 10px;
+        transition: opacity .3s;
+
+        &:hover{
+            opacity: .7;
+        }            
+    };
+
+    .tag-filter-recurrent ::after{
+        content: '';
+        display: block;
+        width: 65px;
+        margin: 0 auto;
+        border-bottom: 8px solid ${props => props.theme.colors.warning}
+    };
+
+    .tag-filter-eventual ::after{
+        content: '';
+        display: block;
+        width: 65px;
+        margin: 0 auto;
+        border-bottom: 8px solid ${props => props.theme.colors.success}
+    };
+`;
