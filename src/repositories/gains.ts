6b16d5442319d2fdf60ea2623d65fda270ba1cad
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2021-02-03 09:56:12 -0300

    Update lesson 5.1

---
+++ src/repositories/gains.ts
@@ -0,0 +1,8 @@
+export default [
+    {"description":"Salário",                 "amount": "2500.00", "type":"entrada", "frequency": "recorrente", "date":"2021-01-10"},
+    {"description":"Freela",                  "amount": "150.13",  "type":"entrada", "frequency": "eventual", "date":"2021-01-12"},
+    {"description":"Freela",                  "amount": "230.90",  "type":"entrada", "frequency": "eventual", "date":"2021-01-21"},
+    {"description":"Salário",                 "amount": "2500.00", "type":"entrada", "frequency": "recorrente", "date":"2021-02-10"},
+    {"description":"Freela App",              "amount": "320.78",  "type":"entrada", "frequency": "eventual", "date":"2021-02-12"},
+    {"description":"Uber",                    "amount": "389.90",  "type":"entrada", "frequency": "eventual", "date":"2021-02-12"},
+]
