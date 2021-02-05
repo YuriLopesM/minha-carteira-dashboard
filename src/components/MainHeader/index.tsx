d257195333ec0c9b8154f95073e60c15791a7da8
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2021-02-05 10:37:45 -0300

    Added Light / Dark theme - Lesson 7

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,4 +1,6 @@
-import React, {useMemo} from 'react';
+import React, {useMemo, useState} from 'react';
+
+import { useTheme} from '../../hooks/theme'
 
 import Toggle from '../Toggle';
 import emojis from '../../utils/emojis'
@@ -11,13 +13,27 @@ import {
 
 
 const MainHeader: React.FC = () => {
+    const { toggleTheme, theme } = useTheme();
+
+    const [darkTheme, setDarkTheme] = useState(() => theme.title === 'dark' ? true : false)
+    
+    const handleChangeTheme = () => {
+        setDarkTheme(!darkTheme);
+        toggleTheme();
+    }
+
     const emoji = useMemo(() => {
         const indice = Math.floor(Math.random() * emojis.length)
         return emojis[indice];
     },[])
     return (
         <Container>
-            <Toggle />
+            <Toggle
+                labelLeft="Light"
+                labelRight="Dark"
+                checked={darkTheme}
+                onChange={handleChangeTheme}
+            />
 
                 <Profile>
                     <Welcome>Olá, {emoji}</Welcome>
