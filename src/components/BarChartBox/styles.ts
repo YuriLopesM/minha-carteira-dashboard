711266a3b26c1e41807a5151b062c1052dfa3d1d
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2021-02-08 13:28:57 -0300

    Update lesson 9.8

--- src/components/BarChartBox/styles.ts
+++ src/components/BarChartBox/styles.ts
@@ -1,89 +1,109 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 interface ILegendProps {
     color: string;
 }
 
-
-export const Container = styled.div`
-    width: 49%;
-    height: 260px;
-
-    margin: 10px 0;
-    
-    display: flex;
-
-    border-radius: 7px; 
-
-    background-color: ${props => props.theme.colors.tertiary};
-    color: ${props => props.theme.colors.white};
-`;
-
-export const SideLeft = styled.aside`
-    width: 60%;
-    padding: 30px 20px;
-
-    > h2 {
-        margin-bottom: 57px;
+const animate = keyframes`
+    0% {
+        transform: translateX(100px);
+        opacity: 0;
+    }
+    50%{
+        opacity: .3;
+    }
+    100%{
+        transform: translateX(0px);
+        opacity: 1;
     }
 `;
+
+export const Container = styled.div`
+    width: 48%;
+    min-height: 260px;
+    margin: 10px 0;
+    background-color: ${props => props.theme.colors.tertiary};
+    color: ${props => props.theme.colors.white};
+    
+    border-radius: 7px;
+    display: flex;
+    animation: ${animate} .5s;
+    @media(max-width: 1200px){
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: auto;
+    }
+`;
+
+export const SideLeft = styled.aside`    
+    flex: 1;
+    padding: 30px 20px;
+    > h2 {
+        padding-left: 16px;
+        margin-bottom: 10px;
+    }
+`;
+
 
 export const LegendContainer = styled.ul`
     list-style: none;
-
-    height: 120px;
-    padding: 5px;
+    
+    height: 175px; 
+    padding-right: 15px;
     overflow-y: scroll;
-
-    ::-webkit-scrollbar{
-        width: 7px;
+    ::-webkit-scrollbar {
+        width: 10px;
     }
-
-    ::-webkit-scrollbar-thumb{
+    ::-webkit-scrollbar-thumb {
         background-color: ${props => props.theme.colors.secondary};
         border-radius: 10px;
     }
-
-    ::-webkit-scrollbar-track{
-        background-color: none;
+    ::-webkit-scrollbar-track {
+        background-color: ${props => props.theme.colors.tertiary};
     }
     
+    @media(max-width: 1200px){
+        display: flex;
+        height: auto; 
+    }
 `;
 
 export const Legend = styled.li<ILegendProps>`
     display: flex;
     align-items: center;
-
-    margin-bottom: 12px;
-
-    font-size: 16px;
-
-
+    margin-bottom: 7px;  
+    padding-left: 16px;  
     > div {
         background-color: ${props => props.color};
-
-        width: 55px;
+        width: 40px;
         height: 40px;
         border-radius: 5px;
-
-        font-size: 16px;
-        font-weight: 500;
-
+        
+        font-size: 14px;        
         line-height: 40px;
         text-align: center;
     }
-
     > span {
-        font-weight: 500;
-        margin-left: 7px;
+        margin-left: 5px;
+    }
+    @media(max-width: 1200px){
+        > div {
+            width: 30px;
+            height: 30px;
+            
+            font-size: 10px;        
+            line-height: 30px;
+        }
     }
 `;
 
-export const SideRight = styled.main`
-    width: 40%;
-    height: 250px;
 
-    display: flex;
-    justify-content: center;
-    padding: 50px 30px 10px 0px
+export const SideRight = styled.main`
+   flex: 1;
+   min-height: 150px;
+   display: flex;
+   justify-content: center;
+   padding-top: 35px;
+     
 `;
