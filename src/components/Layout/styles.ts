7543c9da44e7d4092dc00d4c1deb4d2b9ba083cd
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2021-02-08 10:01:14 -0300

    Revert "Revert "Update lesson 9.2""
    This reverts commit 3c832f9b887d90d076b7311f15f096d282e49087.

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -15,5 +15,13 @@ export const Grid = styled.div`
     'AS CT';
     overflow-x: hidden;
     height: 100vh;
+
+    @media(max-width: 600px){
+        grid-template-columns: 100%;
+        grid-template-rows: 70px auto;
+        grid-template-areas:
+          'MH'
+          'CT';
+    }
     
 `;
