92a93f47f43312376f42710e2559ee4c3b5e111b
Yuri Lopes Machado <yurilopesmachado@hotmail.com>
2021-02-08 08:39:03 -0300

    Update lesson 8.4

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+
+import { useAuth } from '../../hooks/auth'
 
 import {
     Container,
@@ -13,17 +15,32 @@ import {
 } from './styles'
 
 const SignIn: React.FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const { signIn } = useAuth();
+
     return(
         <Container>
             <Logo>
                 <img src={logoImg} alt="Minha Carteira" />
                 <h2>Minha Carteira</h2>
             </Logo>
-            <Form onSubmit={() => {}}>
+            <Form onSubmit={() => signIn(email, password)}>
                 <FormTitle>Entrar</FormTitle>
 
-                <Input type="email" placeholder="Email" required />
-                <Input type="password" placeholder="Senha" required />
+                <Input 
+                    type="email" 
+                    placeholder="Email" 
+                    required 
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input 
+                    type="password" 
+                    placeholder="Senha" 
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
                 <Button type="submit">Acessar</Button>
             </Form>
